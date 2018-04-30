7bb6743c05f996e78b1e2e00528c341a3131fff7
sibappcom <38854752+sibappcom@users.noreply.github.com>
2018-04-30 14:47:49 +0430

    add data

---
+++ data.js
@@ -0,0 +1,23 @@
+
+var data = {
+  title : "روز معلم مبارک",
+  description: "به مناسبت روز معلم کد تخفیف زیر برای شما برای شما برای شما",
+  applications: [
+    {
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/016/small/stapler-2ibuvn.jpg",
+      link: "https://new.sibapp.com/applications/ilearnpiano",
+      name: "iLearnPiano"
+    }
+  ],
+  vochers: [
+    {
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/001/006/small/stapler-7tnhr0.jpg",
+      name: "دیجی کالا",
+      description: "کد تخفیف فلان را حین پرداخت وارد نمایید",
+      code: "SIBAPP-DIGI"
+
+    }
+  ]
+
+
+};
