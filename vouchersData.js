dd872b7b512fc76f91b325dcfa9cddad738d776b
sibappcom <38854752+sibappcom@users.noreply.github.com>
2018-05-01 18:39:34 +0430

    final2

--- vouchersData.js
+++ vouchersData.js
@@ -23,44 +23,107 @@ var data = {
       link: "https://new.sibapp.com/applications/wikilinks",
       name: "WikiLinks"
     },
-    {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/503/small/stapler-1iqm3m.jpg",
+	{
+	icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/503/small/stapler-1iqm3m.jpg",
       link: "https://new.sibapp.com/applications/slmb4iphone",
       name: "reading trainer for iphone"
-    }
-
+	  },
+	  {
+	  	icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/004/984/small/stapler-pqahxy.jpg",
+      link: "https://new.sibapp.com/applications/alphabet",
+      name: "English Alphabet"
+	   },
+	{
+	 icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/005/142/small/stapler-ot0pfn.jpg",
+      link: "https://new.sibapp.com/applications/newdictbox",
+      name: "قاموس"
+	 },
+	{
+	 icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/003/235/small/stapler-nhhubb.jpg",
+      link: "https://new.sibapp.com/applications/livresaffaires",
+      name: "IT Books in itReads"
+	  },
+	  {
+	  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/002/909/small/stapler-0nvqwd.jpg",
+      link: "https://new.sibapp.com/applications/studytime",
+      name: "Brain Wave Study Smart"
+	  },
+	    {
+			  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/037/small/stapler-yswsez.jpg",
+      link: "https://new.sibapp.com/applications/starwalkkids",
+      name: "Star Walk™ Kids"
+	  },
+	  {
+	  			  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/001/039/small/stapler-kutx7s.jpg",
+      link: "https://new.sibapp.com/applications/tabs",
+      name: "Tabs & Chords by Ultimate Guitar"
+	  },
+	  {
+	   icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/009/420/small/stapler-iu2sng.jpg",
+      link: "https://new.sibapp.com/applications/khan-academy",
+      name: "Khan Academy"
+	    },
+	  {
+	   icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/006/294/small/stapler-cumxcr.jpg",
+      link: "https://new.sibapp.com/applications/academy",
+      name: "Academy"
+	  },
+	  {
+	  	   icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/003/384/small/stapler-tamxyu.jpg",
+      link: "https://new.sibapp.com/applications/kmf3",
+      name: "Kids Math Fun"
+	   },
+	   {
+	  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/369/small/stapler-awi4dt.jpg",
+      link: "https://new.sibapp.com/applications/curious",
+      name: "Curious"
+	  },
+	  {
+	  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/005/712/small/stapler-veabc6.jpg",
+      link: "https://new.sibapp.com/applications/juddingstoday-ios",
+      name: "Juddings Today"
+	    },
+	  {
+	  	  icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/005/712/small/stapler-veabc6.jpg",
+      link: "https://new.sibapp.com/applications/ilearnpiano",
+      name: "iLearn Piano"
+	  },
   ],
   vouchers: [
     {
       icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/009/691/small/b3r3djm7otd67tgbkxib.jpg",
       name: "نصرت",
+	  Link: https://new.sibapp.com/applications/%D9%86%D8%B3%D8%AE%D9%87-%DA%A9%D8%A7%D9%85%D9%84-%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B2%D8%A8%D8%A7%D9%86-%D9%86%D8%B5%D8%B1%D8%AA
       description: "20% تخفیف روی پکیج آموزشی ",
       code: ""
     },
     {
       icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
       name: "استاد کت",
+	  Link: https://new.sibapp.com/applications/ostadcat
       description: "50% تخفیف بر روی کلیه پکیج ها",
       code: ""
     },
-    {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
+   { 
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/014/805/small/ic_launcher.png",
       name: "تخته سیاه",
+	  Link: https://new.sibapp.com/applications/takhtesiyaah
       description: "کد تخفیف را هنگام پرداخت وارد نمایید",
-      code: "Sibapp-2018"
-    },
-    {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
+      code: "Sibapp-2018" 
+  },
+  {
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/010/418/small/appicon60x60at3.png",
       name: "یادیز",
+	  Link: https://new.sibapp.com/applications/yadease
       description: "کد تخفیف را هنگام پرداخت وارد نمایید",
-      code: "Sibapp1397"
-    },
-    {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
+      code: "Sibapp1397" 
+	 }, 
+	 {
+	 icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/012/958/small/icon_final.png",
       name: "قبولی",
+	  Link: https://new.sibapp.com/applications/ghabouli
       description: "به 10 نفر اولی که در هر روز اپ را دانلود کنند پکیج رایگان تعلق میگیرد",
-      code: ""
-    }
+      code: "" 
   ]
 
 
