a9003f3f2c36305be568a1a94e0b42251c7fd672
sibappcom <38854752+sibappcom@users.noreply.github.com>
2018-05-01 12:22:07 +0430

    test 3

--- vouchersData.js
+++ vouchersData.js
@@ -1,43 +1,53 @@
-
 var data = {
   mainTitle : "روز معلم مبارک",
   description: "به مناسبت روز معلم کد تخفیف زیر برای شما برای شما برای شما",
-  applicationTitle: "برنامه‌های پیشنهادی سیب اپ",
+  applicationTitle: "برنامه های پیشنهادی سیب اپ",
   applications: [
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/016/small/stapler-2ibuvn.jpg",
-      link: "https://new.sibapp.com/applications/ilearnpiano",
-      name: "iLearnPiano"
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/006/911/small/stapler-lf1faz.jpg",
+      link: "https://new.sibapp.com/applications/nav",
+      name: "Math Pro"
     },
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/016/small/stapler-2ibuvn.jpg",
-      link: "https://new.sibapp.com/applications/ilearnpiano",
-      name: "iLearnPiano"
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/004/985/small/stapler-vhkai7.jpg",
+      link: "https://new.sibapp.com/applications/calc-swift",
+      name: "Calc swift"
     },
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/016/small/stapler-2ibuvn.jpg",
-      link: "https://new.sibapp.com/applications/ilearnpiano",
-      name: "iLearnPiano"
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/006/475/small/stapler-y42jmi.jpg",
+      link: "https://new.sibapp.com/applications/dafuer-farsi-deutsch-wortschatztrainer",
+      name: "DaFür Farsi-Deutsch"
     },
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/000/016/small/stapler-2ibuvn.jpg",
-      link: "https://new.sibapp.com/applications/ilearnpiano",
-      name: "iLearnPiano"
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/003/688/small/stapler-rrdmlp.jpg",
+      link: "https://new.sibapp.com/applications/wikilinks",
+      name: "WikiLinks"
     }
   ],
   vouchers: [
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/001/006/small/stapler-7tnhr0.jpg",
-      name: "دیجی کالا",
-      description: "کد تخفیف فلان را حین پرداخت وارد نمایید",
-      code: "SIBAPP-DIGI"
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/009/691/small/b3r3djm7otd67tgbkxib.jpg",
+      name: "نصرت",
+      description: "20% تخفیف روی پکیج آموزشی ",
+      code: ""
     },
     {
-      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/001/006/small/stapler-7tnhr0.jpg",
-      name: "دیجی کالا",
-      description: "کد تخفیف فلان را حین پرداخت وارد نمایید",
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/014/091/small/Haya_icon.png",
+      name: "آموزش مکالمه ۳۰+ زبان هایا | آفلاین + تلفظ",
+      description: "کاهش قیمت پکیج آموزشی از 14500 تومان به 10000 تومان",
       code: ""
-    }
+    },
+   { 
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
+      name: "انگلیسی در سفر",
+      description: "کد تخفیف فلان را حین پرداخت وارد نمایید",
+      code: "" 
+  },
+  {
+      icon: "https://cdn.sibapp.com/attachments/Sibapp/Domains/ApplicationDomain/Models/Application/icons/000/013/607/small/Ostadcat%20(Profile)%20copy.png",
+      name: "انگلیسی در سفر",
+      description: "کد تخفیف فلان را حین پرداخت وارد نمایید",
+      code: "" 
   ]
 
 
